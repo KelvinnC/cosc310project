45109dc9b2d652db420ca0e527cb180f26fc2139
bradc <brad.cocar@gmail.com>
2025-10-20 16:15:01 -0700

    Created user module endpoints for creating, updating, deleting, and getting users

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers.movies import router as movies_router
+from app.routers.user_endpoints import router as users_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ def root():
         return {"message": "Hello Movie World!"}
 
 app.include_router(movies_router)
+app.include_router(users_router)
