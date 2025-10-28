8b3b55fd06d90ae0dea5d81f458c3a1574ec9066
bradapc <brad.cocar@gmail.com>
2025-10-28 10:13:04 -0700

    Added warning and unwarning user modules and associated tests

--- backend/app/routers/user_endpoints.py
+++ backend/app/routers/user_endpoints.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, status, Depends, Request
+from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
 from app.schemas.user import User, UserCreate, UserUpdate
 from app.services.user_service import list_users, get_user_by_id, update_user, create_user, delete_user
+from app.services.penalty_service import warn_user, unwarn_user
+from app.middleware.auth_middleware import jwt_auth_dependency
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -25,3 +27,15 @@ def put_user(user_id: str, payload: UserUpdate):
 def remove_user(user_id: str):
     delete_user(user_id)
     return None
+
+@router.patch("/{user_id}/warn", response_model=User)
+def add_user_warning(user_id: str, current_user: dict = Depends(jwt_auth_dependency)):
+    if current_user.get("role") != "admin":
+        raise HTTPException(403, detail="Unauthorized action")
+    return warn_user(user_id)
+
+@router.patch("/{user_id}/unwarn", response_model=User)
+def remove_user_warning(user_id: str, current_user: dict = Depends(jwt_auth_dependency)):
+    if current_user.get("role") != "admin":
+        raise HTTPException(403, detail="Unauthorized action")
+    return unwarn_user(user_id)
---
+++ backend/tests/test_penalty_service.py
@@ -0,0 +1,48 @@
+import pytest
+from app.services.penalty_service import warn_user, unwarn_user
+from fastapi import HTTPException
+from app.schemas.user import User
+
+@pytest.fixture
+def user_object():
+    return User(
+        id="1234",
+        username="testmovielover",
+        hashed_password="$2y$10$b1d2DgDhd1bdRbiwSqfZs.MhtyNCMHaYbQp3.6D3ngYLQ9ySTM/HO",
+        role="user",
+        created_at="2025-10-20 16:23:33.447838",
+        active=True,
+        warnings=0
+    )
+
+def test_penalty_service_valid_warn(mocker, user_object):
+    mocker.patch("app.services.penalty_service.get_user_by_id_unsafe",
+                 return_value=user_object)
+    mocker.patch("app.services.penalty_service.save_all")
+    assert user_object.warnings == 0
+    user = warn_user(user_object.id)
+    assert user.warnings == 1
+
+def test_penalty_service_warn_invalid_user(mocker, user_object):
+    mocker.patch("app.services.penalty_service.get_user_by_id_unsafe",
+                 return_value=None)
+    mocker.patch("app.services.penalty_service.save_all")
+    with pytest.raises(HTTPException) as ex:
+        warn_user("invalidid")
+    assert ex.value.status_code == 404
+
+def test_penalty_service_valid_unwarn(mocker, user_object):
+    mocker.patch("app.services.penalty_service.get_user_by_id_unsafe",
+                 return_value=user_object)
+    mocker.patch("app.services.penalty_service.save_all")
+    assert user_object.warnings == 0
+    user = unwarn_user(user_object.id)
+    assert user.warnings == -1
+
+def test_penalty_service_unwarn_invalid_user(mocker, user_object):
+    mocker.patch("app.services.penalty_service.get_user_by_id_unsafe",
+                 return_value=None)
+    mocker.patch("app.services.penalty_service.save_all")
+    with pytest.raises(HTTPException) as ex:
+        unwarn_user("invalidid")
+    assert ex.value.status_code == 404
