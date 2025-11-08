13d6aac28244d6278175c71b004f43cbf7fd5c65
bradc <brad.cocar@gmail.com>
2025-11-08 15:08:39 -0800

    Added get_warned_users() and tests

--- backend/tests/test_admin_service.py
+++ backend/tests/test_admin_service.py
@@ -1,5 +1,29 @@
 import pytest
-from app.services.admin_service import load_all, get_user_count
+from app.services.admin_service import load_all, get_user_count, get_warned_users
+from app.schemas.user import User
+
+@pytest.fixture
+def warned_users():
+    return [
+  {
+    "id": "1234",
+    "username": "admin",
+    "hashed_password": "H62THmnBQCLD",
+    "role": "admin",
+    "created_at": "2025-10-27T21:34:34.307701",
+    "active": True,
+    "warnings": 0
+  },
+  {
+    "id": "abcd",
+    "username": "testuser",
+    "hashed_password": "mnjvhMkYV45e",
+    "role": "user",
+    "created_at": "2025-10-27T21:01:59.674752",
+    "active": False,
+    "warnings": 5
+  }
+]
 
 def test_get_user_count_one_user(mocker, user_data):
     mocker.patch("app.services.admin_service.load_all", return_value=[user_data])
@@ -10,3 +34,17 @@ def test_get_user_count_no_users(mocker):
     mocker.patch("app.services.admin_service.load_all", return_value=[])
     total_users = get_user_count()
     assert total_users == 0
+
+def test_get_warned_users(mocker, warned_users):
+    user_objects = [User(**usr) for usr in warned_users]
+    mocker.patch("app.services.admin_service.list_users", return_value=user_objects)
+    total_warned_users = get_warned_users()
+    assert len(total_warned_users) == 1
+    assert total_warned_users[0].warnings == 5
+
+def test_get_warned_users_no_warned_users(mocker, warned_users):
+    user_objects = [User(**usr) for usr in warned_users]
+    user_objects[1].warnings = 0
+    mocker.patch("app.services.admin_service.list_users", return_value=user_objects)
+    total_warned_users = get_warned_users()
+    assert len(total_warned_users) == 0
