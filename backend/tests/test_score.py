a525a56738c74dfc8cd78c40b073b5c7168bebad
dkrabenstein <dkrabenstein@gmail.com>
2025-11-14 17:00:41 -0800

    router added to main

--- backend/tests/test_score.py
+++ backend/tests/test_score.py
@@ -9,7 +9,7 @@ def client():
     with TestClient(app) as client:
         yield client
 
-def test_get_movie_by_id_valid_id(mocker):
+def test_get_score_by_author_id(mocker):
     mocker.patch("app.services.score_service.load_reviews", return_value=[
     {
         "id":  7777,
@@ -28,11 +28,20 @@ def test_get_movie_by_id_valid_id(mocker):
     assert reveiw_score.votes == 6
     assert isinstance(reveiw_score, ReviewScore)
 
-def test_get_score(client):
+def test_get_score(mocker, client):
+    mocker.patch("app.services.score_service.load_reviews", return_value=[
+        {
+            "authorId": "100",
+            "reviewTitle": "Best in Venice. Best CBM of all time? Perhaps.",
+            "rating": 4.5,
+            "votes": 0
+        }
+    ])
     response = client.get("/score/100")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
     assert len(data) == 1
-    assert data[0]["id"] == "6bca4027-ad52-414e-810c-b830571cc07d"
-    assert data[0]["title"] == "Joker"
+    assert data[0]["reviewTitle"] == "Best in Venice. Best CBM of all time? Perhaps."
+    assert data[0]["rating"] == 4.5
+    assert data[0]["votes"] == 0
