1fdfa01769a858e2dc496d5269b9778537221f44
bradc <brad.cocar@gmail.com>
2025-11-17 16:21:39 -0800

    Created user summary service method

--- backend/app/services/user_summary_service.py
+++ backend/app/services/user_summary_service.py
@@ -4,6 +4,7 @@ from typing import List
 from app.services.battle_pair_selector import load_user_battles
 from app.services.user_service import get_user_by_id
 from app.schemas.user import User
+from app.schemas.user import UserSummaryResponse
 
 def get_users_reviews(current_user_id: str) -> List[Review]:
     """Gets the current user's reviews"""
@@ -16,3 +17,10 @@ def get_users_battles(current_user_id: str) -> List[dict]:
 def get_user_object(current_user_id: str) -> User:
     """Gets the current user's user object"""
     return get_user_by_id(current_user_id)
+
+def get_user_summary(current_user_id: str) -> UserSummaryResponse:
+    return UserSummaryResponse(
+        battles=get_users_battles(current_user_id),
+        reviews=get_users_reviews(current_user_id),
+        user=get_user_object(current_user_id)
+    )
