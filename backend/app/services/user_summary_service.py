92d63dbf75fbbe3926e7ef6e03579200cfcde2e9
bradc <brad.cocar@gmail.com>
2025-11-17 16:18:53 -0800

    Created get_users_battles to return the logged in user's vattles

--- backend/app/services/user_summary_service.py
+++ backend/app/services/user_summary_service.py
@@ -1,7 +1,12 @@
 from app.services.review_service import get_reviews_by_author
 from app.schemas.review import Review
 from typing import List
+from app.services.battle_pair_selector import load_user_battles
 
 def get_users_reviews(current_user_id: str) -> List[Review]:
     """Gets the current user's reviews"""
     return get_reviews_by_author(current_user_id)
+
+def get_users_battles(current_user_id: str) -> List[dict]:
+    """Gets the current user's battles"""
+    return load_user_battles(current_user_id)
