9da3d85bd07379dc44c223a20e453d3e58cc9f4d
bradapc <brad.cocar@gmail.com>
2025-11-25 10:43:45 -0800

    Styled register page

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import './register.css'
+import Link from 'next/link';
 
 const page = () => {
   return (
@@ -13,7 +14,7 @@ const page = () => {
         <input type="text" placeholder="username" className="register-textbox"></input>
         <input type="text" placeholder="password" className="register-textbox"></input>
         <button className="register-button" type="submit">Register</button>
-        <p>Already have an account? Sign in instead</p>
+        <p>Already have an account? <Link className="sign-in-link" href="/login">Sign in</Link> instead</p>
       </form>
     </div>
   )
