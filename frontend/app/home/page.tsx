ad154485b07fd3a586db829101ec14ec3bf403d2
bradapc <brad.cocar@gmail.com>
2025-11-26 18:21:03 -0800

    Styled user data and main home page

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -30,17 +30,39 @@ const page = () => {
     <div className="user-dashboard">
         {userData && (
             <div>
-                <h1 className="review-container-title">Your Reviews</h1>
-                <div className="review-container">
-                    {reviews.map((review, idx) => (
-                        <div key={idx} className="review">
-                            <h2 className="review-title">{review["reviewTitle"]}</h2>
-                            <span>Posted on {review["date"]}</span>
-                            <span>Rating {review["rating"]} / 5</span>
-                            <span className="review-body">{review["reviewBody"]}</span>
-                            <span className="review-votes">{review["votes"]} votes</span>
-                        </div>
-                    ))}
+                {user &&
+                <div className="user-div">
+                    <div className="user-container">
+                        <h1 className="review-container-title">Hi {user["username"]}</h1>
+                        <span>You've posted {reviews.length} reviews and participated in {battles.length} battles</span>
+                        <span>Role: {user["role"]}</span>
+                        <span>Warnings: {user['warnings']}</span>
+                        <span>Account created on {user["created_at"]}</span>
+                    </div>
+                </div>
+                }
+                <div>
+                    <h1 className="review-container-title">Your Reviews</h1>
+                    <div className="review-container">
+                        {reviews.map((review, idx) => (
+                            <div key={idx} className="review">
+                                <h2 className="review-title">{review["reviewTitle"]}</h2>
+                                <span>Posted on {review["date"]}</span>
+                                <span>Rating {review["rating"]} / 5</span>
+                                <span className="review-body">{review["reviewBody"]}</span>
+                                <span className="review-votes">{review["votes"]} votes</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h1 className="review-container-title">Your Battles</h1>
+                    <div className="review-container">
+                        {battles.map((battle, idx) => (
+                            <div key={idx} className="review">
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         )}
