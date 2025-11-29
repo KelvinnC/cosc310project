a5991047adb0d28fa9bb942b0d7186ac1ca12db6
Kelvin <kelvinchentheepic@gmail.com>
2025-11-28 22:59:35 -0800

    new pages for movies

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,5 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { useRouter } from 'next/navigation';
-import { useData } from './context';
-import { useEffect } from 'react';
-
-const page = () => {
-  const router = useRouter()
-  const {accessToken} = useData()
-
-  useEffect(() => {
-    if (!accessToken) {
-      router.push("/login")
-  } else {
-      router.push("/home")
-  }
-  }, [accessToken, router])
-
-
-  return (
-    <div>
-      Redirecting...
-    </div>
-  )
+export default function Home() {
+  return <div className="min-h-screen bg-white" />;
 }
-
-export default page
