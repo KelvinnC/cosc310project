a7f755bca5cd06befce6b67595bd031578ee427b
bradc <brad.cocar@gmail.com>
2025-11-30 20:11:41 -0800

    Updated decodeToken for robustness using replacement chars

--- frontend/lib/decodeToken.js
+++ frontend/lib/decodeToken.js
@@ -2,7 +2,7 @@ function decodeToken(token) {
     try {
 
         const parts = token.split('.')
-        const payload = parts[1];
+        const payload = parts[1].replace(/-/g, '+').replace(/_/g, '/');
         const padded = payload + '='.repeat((4 - (payload.length % 4)) % 4);
         const decoded = JSON.parse(atob(padded))
         return decoded
