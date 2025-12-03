e5c1fbfb5e06d8cfc1d6a949108dcd43edfbc4de
Kelvin Chen <kelvinchentheepic@gmail.com>
2025-12-02 17:29:22 -0800

    fixed CSS conflicts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from "next/navigation";
+"use client"
 
-export default function Home() {
-  redirect("/home");
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import { useData } from './context';
+import { useEffect } from 'react';
+
+const page = () => {
+    const router = useRouter()
+    const {accessToken} = useData()
+
+    useEffect(() => {
+        if (!accessToken) {
+            router.push("/login")
+        } else {
+            router.push("/home")
+        }
+    }, [accessToken, router])
+
+
+    return (
+        <div>
+            Redirecting...
+        </div>
+    )
 }
+
+export default page
